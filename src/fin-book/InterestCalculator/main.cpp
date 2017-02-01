84bf87796e6cd623adbbfa1bd77838d60ca2ce92
Vasyl Melnychuk <vmmelnychuk@gmail.com>
2017-02-01 13:16:04 +0100

    add functions for user-input

--- src/fin-book/InterestCalculator/main.cpp
+++ src/fin-book/InterestCalculator/main.cpp
@@ -2,22 +2,30 @@
 #include <cmath>
 using namespace std;
 
-int main() {
-    int principal;
-    double interestRate;
-    int numberOfYears;
-
+void read_user_input(int& principal, double& interestRate, int& numberOfYears) {
     cout << "How much are you investing: ";
     cin >> principal;
     cout << "What is the annual interest rate (%): ";
     cin >> interestRate;
     cout << "How long (years): ";
     cin >> numberOfYears;
+}
+
+void print_interest(const double& interest) {
+    cout << "You will earn: " << interest << endl;
+}
+
+int main() {
+    int principal;
+    double interestRate;
+    int numberOfYears;
+
+    read_user_input(principal, interestRate, numberOfYears);
 
     double finalBalance = pow(1.0 + interestRate * 0.01, numberOfYears) * principal;
     double interest = finalBalance - principal;
-
-    cout << "You will earn: " << interest << endl;
-
+    
+    print_interest(interest);
+    
     return 0;
 }
